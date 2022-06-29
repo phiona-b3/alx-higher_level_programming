bc2675173aaae80f7da7c43a37f2f97736a917f9
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-29 15:13:44 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 8):
     for b in range(0, 10):
         if b > a:
-            print(f"{a}", end="")
-            print(f"{b}", end=", ")
+            print("{}".format(a), end="")
+            print("{}".format(b), end=", ")
 print(89)
