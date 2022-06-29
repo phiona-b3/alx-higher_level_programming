80bfe3d88d56e0d27428d1956d7d5ad24fe8584e
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-29 15:04:08 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for n in range(00, 100):
-    if n == 99:
-        print("{:02d}".format(n), end="")
-    else:
-        print("{:02d}".format(n), end=',' + " ")
+for n in range(0, 99):
+    print("{:02d}".format(n), end=", ")
+print(99)
