9c2101b456bf8555d67d86c6ffdc106fcdc43cd7
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-30 12:09:14 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,7 @@ import add_0
 a = 1
 b = 2
 
-c = add_0.add(a,b)
+c = add_0.add(a, b)
 d = "<a value> + <b value> = <add(a, b) value>"
-print("{}".format(d), end =" ")
+print("{}".format(d), end=" ")
 print(c)
