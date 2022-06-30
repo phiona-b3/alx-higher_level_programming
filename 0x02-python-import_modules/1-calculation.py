f7a364ea48bb22e9d9be01a83733024a1629e34a
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-06-30 15:45:15 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """maths functions"""
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
 
