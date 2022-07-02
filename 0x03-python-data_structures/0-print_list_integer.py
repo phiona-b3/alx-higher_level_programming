9cec4d478dbf6f4bb2452b05bc0cca728c8f85c0
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-02 14:12:05 -0700

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     my_list = [1, 2, 3, 4, 5]
-    print(("{}\n"*len(my_list)).format(*my_list))
+    for i in my_list:
+        print("{}".format(i))
