0955420f706606a6d81c775336b3ee81c41df468
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-03 00:16:15 -0700

    Task 5

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -2,5 +2,5 @@
 print_reversed_list_integer = __import__(
         '3-print_reversed_list_integer') .print_reversed_list_integer
 
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
 print_reversed_list_integer(my_list)
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    mystring = my_string.replace("c", " ")
+    mystring = my_string.replace("C", " ")
+    print("{}".format(mystring))
