538503db5938e147cef33e54530bedebed0e66f1
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-03 00:33:55 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    mystring = my_string.replace("c", " ")
-    mystring = my_string.replace("C", " ")
-    print("{}".format(mystring))
+    print(my_string.translate({ord(i): None for i in 'cC'}))
