425a4b6d553640668c2a4356049bdcd2a136a97b
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-04 09:59:44 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if (idx < 0):
-            print("None")
-        elif (idx > len(my_list)):
-            print("None")
+    items = len(my_list) - 1
+    if idx < 0 or idx > items:
+        return None
     else:
-        print("Element at index {:d} is {}".format(idx, my_list))
+        return my_list[idx]
