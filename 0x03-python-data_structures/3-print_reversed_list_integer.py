e22f1dcd08fe80cf7cec3bb759d58469e475ef57
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-05 22:08:25 -0700

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = reversed(my_list)
-    for item in new_list:
-        print("{:d}".format(item))
+    for i in reversed(my_list):
+        print("{:d}".format(i))
