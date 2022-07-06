03902ed772c9e7d638f410718fe4506b8cf97a7a
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-06 08:22:53 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == 0:
+    if len(sentence) == 0:
         return None
     else:
         new_tuple = (len(sentence), sentence[0])
         return new_tuple
+
+
