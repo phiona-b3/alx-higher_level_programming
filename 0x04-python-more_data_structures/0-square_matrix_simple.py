36ee37498073686245db0c51ec4b467e9458dd4b
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-07 13:27:50 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     for row in matrix:
-        for col in matrix:
-            return map(lambda x: x**2, matrix)
+        for col in row:
+            print("{:d}".format(col**2))
+    print()
