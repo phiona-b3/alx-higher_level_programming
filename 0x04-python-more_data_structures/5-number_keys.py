e8d202dbbc14831177339cc79f55921a4a4ab368
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-13 13:57:53 -0700

    Task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    count = 0
-    for key, value in a_dictionary.items():
-        count += 1
-        return count
+    return len(a_dictionary.keys())
