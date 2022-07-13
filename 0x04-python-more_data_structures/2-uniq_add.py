ae4b0843e2888d6c8451f400f5c47751832bff55
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-13 13:47:43 -0700

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_list = []
-    for x in my_list:
-        if x not in unique_list:
-            unique_list.append(x)
-        else:
-            pass
-        s = sum(unique_list)
-        return s
+    res = 0
+    for i in set(my_list):
+        res += i
+        
+    return res
