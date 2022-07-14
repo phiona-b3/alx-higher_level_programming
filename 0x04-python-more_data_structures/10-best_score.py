ea9f5ed010f85900c53331f0c9bab04a5c6ad804
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-14 10:02:35 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,8 @@ def best_score(a_dictionary):
     max_key = ""
     if a_dictionary is None:
         return None
-        for key in a_dictionary.keys():
-            if a_dictionary[key] > max_val:
-                max_val = a_dictionary[key]
-                max_key = key
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > max_val:
+            max_val = a_dictionary[key]
+            max_key = key
+    return max_key
