aa698bd12844c070eb33e6d7ae14c1eb7facd90f
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-14 09:07:33 -0700

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary
-    for key in a_new_dict:
+    new_dict = a_dictionary.copy()
+    for key in new_dict.keys():
         new_dict[key] *= 2
-        return new_dict
+    return new_dict
