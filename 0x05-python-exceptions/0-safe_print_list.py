621fe79952219d1d578773474fc062c098e6492c
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-18 11:16:09 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in my_list:    
+    for i in my_list:
         try:
             print("{}".format(i), end="")
-        except:
-            print("None")
+        except TypeError:
+            print()
