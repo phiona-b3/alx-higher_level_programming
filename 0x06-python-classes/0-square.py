6354a952ab852da3892155d9fa1903569fe2e946
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-19 09:59:07 -0700

    Task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """One can create their own class using the word
-followed by name"""
+followed by name.
+in this task, it is creating an empty class square"""
 class Square:
     pass
