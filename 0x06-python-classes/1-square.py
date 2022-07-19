785a4602c0781977804a28b214c5f3026516c3c1
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-19 14:53:01 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Writes a class called Square and
+Private instance attribute: size"""
+
+
 class Square:
+    """the class is Square"""
     def __init__(self, size):
+        """self.__size makes it a private
+        instance attribute"""
         self.__size = size
