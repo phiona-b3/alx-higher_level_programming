5db8bb974b9528de40c42e60f08a7f48c4631564
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-19 13:26:39 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """python3 -c 'print(__import__("my_module").__doc__)'
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-    python3 -c 'print(__import__("my_module").my_function.__doc__)'
-    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-    """
+    """class:
+        string
+        Attributes:
+        pass executes nothing
+        Returns an empty class"""
     pass
