42ffe2210e12721bd76791f1465bfdc549d7e6c7
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-19 14:30:48 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,5 @@
-"""Module to Write an empty class Square that 
-defines a square"""
 #!/usr/bin/python3
 """Returns class Square as an empty class"""
+"""Square is the name of the class"""
 class Square:
-    """Square is the name of the class"""
-    """pass command executes nothing and returns 
-    empty class"""
     pass
