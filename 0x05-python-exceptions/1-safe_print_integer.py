198f8ebe9596c4d9b24cfa30443092b5cd4714bb
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-20 14:54:55 -0700

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int:
-                print("{:d}". format(value))
-    except:
-        print("{}". format())
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
