ea2f91091bb82691e68bde2069e07fbeea134b34
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-07-25 22:56:24 -0700

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-"""Define a class called Square"""
+"""create a class called Square"""
 
 
 class Square:
     """create a private instance attribute"""
-    area = 0
     def __init__(self, size=0):
         self.__size = size
         """ check for the type of value in size"""
@@ -15,11 +13,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-
-    """define a function called area"""
-
-
-@classmethod
-def area(self):
-    """return the area"""
-    return self.__size ** 2
+    def area(self):
+        """return the area"""
+        return self.__size ** 2
