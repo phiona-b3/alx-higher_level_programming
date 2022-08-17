c5c00d0720583a0f111c30afd6358d72c1bb9d8f
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-08-17 22:50:36 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return(ord('a') <= ord(c) <= ord('z'))
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
