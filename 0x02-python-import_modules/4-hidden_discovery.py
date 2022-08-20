8123dfe5e0225c626994673cd852a2320353cbe9
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-08-20 15:25:39 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ = "__main__":
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
-            print("{:}".format(name))
+            print("{:s}".format(name))
