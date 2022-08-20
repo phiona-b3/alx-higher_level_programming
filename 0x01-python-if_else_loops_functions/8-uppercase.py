377afe59eeb8ad55a332fc0e3b74c2f260cfd8b1
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-08-20 10:46:10 -0700

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    newstr = ""
-    for i in range(0, len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            upper = chr((ord(str[i]) - 32))
-        else:
-            upper = str[i]
-        newstr += upper
-        print("{:s}".format(newstr))
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end='')
+    print("")
