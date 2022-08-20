7fd608bc3c3cde725a5f80473217496ab7a65b2c
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-08-20 14:41:43 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ count = len(sys.argv) - 1
 if count == 0:
     print("{} arguments.".format(count))
 elif count == 1:
-    print("{} argument: ".format(count))
+    print("{} argument:".format(count))
 else:
-    print("{} arguments: ".format(count))
+    print("{} arguments:".format(count))
 for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
