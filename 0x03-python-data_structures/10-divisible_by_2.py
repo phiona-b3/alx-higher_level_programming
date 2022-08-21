4914fb21e895df86cf7e9a25229efd09bc9c6126
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-08-21 06:54:06 -0700

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(my_list):
-        if i % 2 == 0:
-            return True
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if new_list[i] % 2 == 0:
+            new_list[i] = True
         else:
-            return False
+            new_list[i] = False
+    return new_list
