390101f10d07c58f204f77fa8bf62cefbb71d040
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-10-04 13:38:54 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let maxi = 0;
-const arg = process.argv.slice(2);
-if (arg.length > 1) {
-  arg.sort();
-  maxi = arg[arg.length - 2];
+if (process.argv.length < 4) {
+  console.log('0');
+} else {
+  const data = process.argv.slice(2);
+  data.sort((a, b) => b - a);
+  console.log(data[1]);
 }
-console.log(maxi);
