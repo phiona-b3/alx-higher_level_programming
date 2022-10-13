ada1fa0728a3733561d28e3440e2271f38219d3a
phiona-b3 <phiona030303@gmail.com>
2022-10-13 03:52:01 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" a script that is safe from MySQL injections"""
+"""a script that is safe from MySQL injections"""
 import MySQLdb
 import sys
 
 if __name__ = "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cursor = db.cursor
 
-    cursor.execute("SELECT * FROM states WHERE name \
-            LIKE %s ORDER BY id ASC", (sys.argv[4], ))
-    query_rows = cursor.fetchall
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name=%s \
+                 ORDER BY id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall
 
     for row in query_rows:
         print(row)
 
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
