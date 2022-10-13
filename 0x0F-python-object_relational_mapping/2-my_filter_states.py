2c41cc0456bb232f76872167908f089327573801
phiona-b3 <phiona030303@gmail.com>
2022-10-13 00:44:41 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 import sys
 
