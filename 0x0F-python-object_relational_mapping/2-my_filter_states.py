9f66975b22eea6761e56d1386624971183ca49cf
phiona-b3 <phiona030303@gmail.com>
2022-10-13 00:43:11 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
                            db=sys.argv[3])
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY \
-            id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name= '{}' ORDER BY \
+            id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
 
     for row in query_rows:
-        if row[1] == argv[4]:
+        if row[1] == sys.argv[4]:
             print(row)
 
     cur.close()
-    conn.close()
+    conn.closei()
