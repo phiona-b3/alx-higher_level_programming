715cedc539f9df4196f87dae11fd104d1032228f
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-10-13 10:49:01 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all
-values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""Displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument"""
 import MySQLdb
 import sys
 
@@ -10,16 +10,21 @@ if __name__ == "__main__":
                            port=3306,
                            user=sys.argv[1],
                            passwd=sys.argv[2],
-                           db=sys.argv[3])
+                           db=sys.argv[3],
+                           charset="utf8")
+    # Start cursor
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name= '{}' ORDER BY \
-            id ASC".format(sys.argv[4]))
+    # Query
+    cur.execute("""SELECT * FROM states \
+        WHERE name= '{}' ORDER BY id ASC""".format(sys.argv[4]))
     query_rows = cur.fetchall()
 
+    # Print query
     for row in query_rows:
         if row[1] == sys.argv[4]:
             print(row)
 
+    # Close cursor
     cur.close()
-    conn.closei()
+    conn.close()
