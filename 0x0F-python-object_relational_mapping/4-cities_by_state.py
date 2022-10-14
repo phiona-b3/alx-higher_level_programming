fd71d19d139045084ec4668344dada3181330e5b
phiona-b3 <phiona030303@gmail.com>
2022-10-14 01:39:23 -0700

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,9 +8,10 @@ if __name__ = "__main__":
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3],
+                         charset="utf8")
     cursor = db.cursor()
-    cusor.execute("SELECT cities.id, cities.name, states.name \
+    cursor.execute("SELECT cities.id, cities.name, states.name \
             FROM cities, states WHERE cities.state_id = states.id \
             ORDER BY cities.id ASC")
     query_rows = cursor.fetchall()
