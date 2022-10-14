8e5ba5ffda2bfc8253c0579fef66ca2690b01643
phiona-b3 <phiona030303@gmail.com>
2022-10-14 01:30:26 -0700

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,7 @@ if __name__ = "__main__":
                            db=sys.argv[3])
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name=%s \
-                 ORDER BY id ASC", (sys.argv[4], ))
+    cur.execute("SELECT * FROM states WHERE name=%s", (sys.argv[4], ))
     query_rows = cur.fetchall
 
     for row in query_rows:
