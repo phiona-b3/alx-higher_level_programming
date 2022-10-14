4d41f081d97353b86e9741c79ad3fb0ae5f6bce4
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-10-14 13:02:03 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=sys.argv[2],
+                         user=sys.argv[3],
                          passwd=sys.argv[1],
                          db=sys.argv[3])
 
