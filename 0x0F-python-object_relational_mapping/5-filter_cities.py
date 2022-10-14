0e49e2042a67a3951eae26f76eb6674c934b8969
phiona-b3 <phiona030303@gmail.com>
2022-10-14 02:30:24 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
+                         user=argv[2],
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
