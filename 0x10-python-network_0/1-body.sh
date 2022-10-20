8ba4ddad82b39133b8c41eeb5b76f6292f8e7d59
phiona-b3 <phiona030303@gmail.com>
2022-10-20 13:04:52 -0700

    Task 0-3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a GET request to the
-#URL, and displays the body of the response
+#takes in a URL, sends a GET request to the URL
 curl -sL "$1"
