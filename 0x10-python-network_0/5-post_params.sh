0af38a836e59ff074e7c5c5f6ba190caf9aa002a
phiona-b3 <phiona030303@gmail.com>
2022-10-20 13:28:00 -0700

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a POST request to the passed URL,
-curl -sX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
