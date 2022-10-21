4372c027556562750ea2dcb3908f90dbbd61f5b1
phiona-b3 <phiona030303@gmail.com>
2022-10-21 15:13:00 -0700

    Task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" uses the GitHub API to display your id"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    req = requests.get('https://api.github.com/user', auth=(username, password))
+    print(req.json().get('id'))
