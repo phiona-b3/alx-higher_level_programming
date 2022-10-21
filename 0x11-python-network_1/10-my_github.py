98951000f9a96dacd6832c4586c75861b8f043ff
phiona-b3 <phiona030303@gmail.com>
2022-10-21 15:17:19 -0700

    Task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,6 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    req = requests.get('https://api.github.com/user', auth=(username, password))
+    req = requests.get('https://api.github.com/user',
+                       auth=(username, password))
     print(req.json().get('id'))
