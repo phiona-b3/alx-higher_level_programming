747747e5e9a7d2bbc3fc086ebd37fb97e8971072
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-10-22 01:18:41 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Search API """
-import requests
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        a_dict = {'q': ''}
-    elif len(sys.argv) > 1:
-        a_dict = {'q': sys.argv[1]}
-
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
-        if req.json():
-            print('[{}] {}'.format(req.json().get('id'),
-                                   req.json().get('name')))
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
         else:
-            print('No result')
-
-    except ValueError:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
         print("Not a valid JSON")
