783bce9e0d01b06b33372c7c654b92b503f006a2
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-16 00:20:40 +0300

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
+
+const requests = require('request');
 const url = process.argv[2];
-const newDict = {};
-request.get(url, function (err, response, body) {
-  if (err == null) {
-    const todos = JSON.parse(body);
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i].completed === true) {
-        if (newDict[todos[i].userId] === undefined) { newDict[todos[i].userId] = 0; }
-        newDict[todos[i].userId] += 1;
+
+requests.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const users = JSON.parse(body).reduce((accumulator, currentValue) => {
+    if (currentValue.completed) {
+      if (accumulator[currentValue.userId]) {
+        accumulator[currentValue.userId]++;
+      } else {
+        accumulator[currentValue.userId] = 1;
       }
     }
-    console.log(newDict);
-  }
+    return accumulator;
+  }, {});
+  console.log(users);
 });
