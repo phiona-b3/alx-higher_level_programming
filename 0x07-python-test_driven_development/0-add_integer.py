c67e668eb3df8b58cb204e92fc238fe1cf4408e6
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-12-20 19:10:21 +0300

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """function that adds two integer numbers"""
+    """
+''add_integer(a, b=98)'' add two integers numbers.
+You can use a integer or float number like parameter,
+once inside of the function each parameter us cast to
+integer. If some parameter is diffrent type from int
+or float the functione raise TypeError with a message.
+    """
     if type(a) in [int, float]:
         num1 = int(a)
     else:
@@ -12,5 +18,4 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     if type(num1) is int and type(num2) is int:
-
         return num1 + num2
