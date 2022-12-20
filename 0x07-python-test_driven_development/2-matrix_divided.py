d07337732b1ac87bf410f6760d59c34e5463a16b
phiona-b3 <phiona030303@gmail.com>
2022-12-20 12:21:27 +0300

    Task 1, 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """matrix divsion"""
+
+    if type(div) not in [int,float]:
+        raise TypeError("div must be a number")
+    elif div is 0:
+        raise ZeroDivisionError("division by zero")
+
+    if type(matrix) is not list or len(matrix) is 0:
+        raise TypeError("matrix must be a matrix (list of lists) of \ integers/floats")
+    else:
+        for i in matrix:
+            if type(i) is not list:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            else:
+                for j in i:
+                    if type(j) not in [int, float]:
+                        raise TypeError ("matrix must be a matrix (list of lists) of integers/floats")
+    
+        r_len = len(matrix[0])
+        for n in range(len(matrix)):
+            if len(matrix[n])  != r_len:
+                raise TypeError("Each row of the matrix must have the same size")
+
+        New_matrix = [[round((el / div), 2) for el in i] for i in matrix]
+    
+    return New_matrix
