0bc7dcb28a39d8c9d7439c0577f94229b031d059
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-12-20 19:11:56 +0300

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+""" File name : 0-add_integer.py
+    Integers addition: function that adds 2 integers
+    Prototype: def add_integer(a, b=98)
+    You are not allowed to import any module
+"""
+
+
 def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-''add_integer(a, b=98)'' add two integers numbers.
-You can use a integer or float number like parameter,
-once inside of the function each parameter us cast to
-integer. If some parameter is diffrent type from int
-or float the functione raise TypeError with a message.
-    """
-    if type(a) in [int, float]:
-        num1 = int(a)
-    else:
+    if (a is None or not isinstance(a, int) and (not isinstance(a, float))):
         raise TypeError("a must be an integer")
-
-    if type(b) in [int, float]:
-        num2 = int(b)
-    else:
+    if (not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    if type(num1) is int and type(num2) is int:
-        return num1 + num2
+    return int(a) + int(b)
