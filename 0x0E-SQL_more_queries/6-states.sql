2826e36e6843f128bf39ad40c146a6d2a7e689b4
phiona-b3 <phiona030303@gmail.com>
2023-01-17 21:35:12 +0300

    More taskks

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
--- create a database and table
-CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
-    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+-- Create another data base and another table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
 );
