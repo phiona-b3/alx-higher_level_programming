c491b15b16858c134ed99b5feadf816021003bc6
phiona-b3 <phiona030303@gmail.com>
2023-01-17 21:31:56 +0300

    kore tasks

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- create table with id not null
+-- Create a table with id not null
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    NAME VARCHAR(256)
+       id INT DEFAULT 1,
+       name VARCHAR(256)
 );
